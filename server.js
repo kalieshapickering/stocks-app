fe59802384178ab371d8f2693e3df4882be15b09
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-15 15:37:32 -0500

    Rerouted root to client/build/index.html

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 console.log(`NODE_ENV is ${process.env.NODE_ENV}`)
 if (process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // Make Public a static folder
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'public'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 }); 
 
 app.listen(PORT, () => {
