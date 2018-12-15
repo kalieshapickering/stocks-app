cbd7543fc4bb81673f0f365b18ace9ff85c3ce80
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-15 15:23:57 -0500

    Corrected sendfile typo

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Make Public a static folder
 app.get('/', function(req, res) {
-  res.sendfile(path.join(__dirname, 'client', 'public'));
+  res.sendFile(path.join(__dirname, 'client', 'public'));
 }); 
 
 app.listen(PORT, () => {
