ada5ff1a369bd993b36c501db8d4d4df8b9e02f5
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-15 14:02:43 -0500

    Specified index.html as static route

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build/index.html"));
 }
 
 app.listen(PORT, () => {
