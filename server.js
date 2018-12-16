ca542081e758858333ede9e21c544e9f35dddc7a
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-16 18:13:55 -0500

    Added static route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use("/static", express.static(path.join(__dirname, 'client/build')));
 }
 
 // use route file
