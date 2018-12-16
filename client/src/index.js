867084eb02ab6922b480af9b93c0156495c13303
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-16 16:37:31 -0500

    Minor adjustments

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
+console.log("Any script at all?")
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
